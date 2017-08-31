0a22fae21f9c9bd5a356d911be4cd18524a802d5
DOST-MIS <DOST-MIS-PC-1@DOST-MIS-PC1.(none)>
2017-08-31 17:31:38 +0800

    creating online monitor for feedbacks

--- spec_func.php
+++ spec_func.php
@@ -55,13 +55,13 @@ ini_set('max_execution_time',0);
     }
     $chartStrCnt = json_encode($chartStrCnt);
     ?>
-    <canvas id="myChart" width="556" height="300" style="width:556px;height:355px;"></canvas>
+    <canvas id="myRateChart" width="556" height="290" style="width:556px;height:355px;"></canvas>
     <script type="text/javascript">
-      var ctx = document.getElementById("myChart");
+      var ctx = document.getElementById("myRateChart");
       var chartStrCnt = <?php echo $chartStrCnt; ?>;
       var label = 'Chart Ratings from '+<?php echo(json_encode($_REQUEST['lgu'])); ?>;
       var myChart = new Chart(ctx, {
-          type: 'bar',
+          type: 'pie',
           data: {
               labels: ["Excellent", "Very Satisfied", 'Satisfied','Fair','Poor'],
               datasets: [{
@@ -93,39 +93,111 @@ ini_set('max_execution_time',0);
     <?php
       $navBarMain = array_unique($dataArr[4]); //Index 4 is the index for the Services
     ?>
-    <div class="sidebar-wrapper" role="navigation">
-      <div class="sidebar-nav navbar-collapse">
-        <ul class="nav">
-          <?php
-            $navBarMain = array_unique($dataArr[4]); //Index 4 is the index for the Services
-            foreach($navBarMain as $key1 => $value1){
-              echo '<li><a href="#" onclick="event.preventDefault()">'.strtoupper($value1).'</a><span class="fa-arrow"></span>';
-              echo '<ul class="nav nav-second-level collapse">';
-                // foreach($dataArr[4] as $key2 => $value2){
-                //   if($value1 == $value2){
-                //     echo '<li>'.$dataArr[5][$key2].'</li>';
-                //     unset($dataArr[4][$key2]);
-                //     unset($dataArr[5][$key3]);
-                //   }
-                // }
-              echo '</ul>';
-              echo '</li>';
+
+    <style media="screen">
+      .myNav{
+        padding-left: 15px;
+      }
+      .myNav > li{
+        list-style: none;
+        padding:5px;
+      }
+    </style>
+
+    <div class="panel-group closeall" id="accordion" role="tablist" aria-multiselect="true">
+      <div class="panel panel-default">
+        <?php
+        $navBarMain = array_unique($dataArr[4]); //Index 4 is the index for the Services
+        foreach($navBarMain as $key1 => $value1){
+          echo '<div class="panel-heading" role="tab" id="heading'.$key1.'" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse'.$key1.'" aria-expanded="true" aria-controls="collapse'.$key1.'">
+            <h4 class="panel-title">
+              <a role="button" href="#collapse'.$key1.'" onclick="event.preventDefault()">
+                '.$value1.'
+              </a>
+            </h4>
+          </div>
+          <div id="collapse'.$key1.'" class="panel-collapse collapse in" role="tabpanel" aria-labelledBy="heading'.$key1.'">
+            <div class="panel-body">';
+            foreach($dataArr[4] as $key2 => $value2){
+              if($value1 == $value2){
+                echo '<p class="btn form-control" style="text-align:left">'.$value2.'</p>';
+                unset($dataArr[4][$key]);   unset($dataArr[5][$key]);
+              }
             }
-          ?>
-        </ul>
+          echo '</div>
+          </div>';
+        }
+         ?>
       </div>
+    </div>
+
+    <div class="panel">
+      <ul class="myNav dropdown-menu" id="side-menu">
+        <?php
+          $navBarMain = array_unique($dataArr[4]); //Index 4 is the index for the Services
+          foreach($navBarMain as $key1 => $value1){
+            echo '<li><a href="#" onclick="event.preventDefault()">'.strtoupper($value1).'</a><span class="fa-arrow"></span>';
+            echo '<ul class="nav nav-second-level collapse">';
+              // foreach($dataArr[4] as $key2 => $value2){
+              //   if($value1 == $value2){
+              //     echo '<li>'.$dataArr[5][$key2].'</li>';
+              //     unset($dataArr[4][$key2]);
+              //     unset($dataArr[5][$key3]);
+              //   }
+              // }
+            echo '</ul>';
+            echo '</li>';
+          }
+        ?>
+      </ul>
     </div>
     <!--BREAK-->
     <div style="overflow-y:auto">
-      <h3>List of Comments</h3>
       <?php
         foreach($dataArr[1] as $key => $value){
           if(strlen($value) > 25)
             $value = substr($value, 0, 20).'...';
-          echo "<button class='btn form-control' style='color:black;text-align:left'><img class='' src='images/clipart/blank-dp.png' style='width:20px;'>".$value."</button>";
+          echo "<button class='btn form-control' style='color:black;text-align:left'><img class='' src='images/clipart/blank-dp.png' style='width:20px;margin-right:10px;'>".$value."</button>";
         }
        ?>
     </div>
+    <!--BREAK-->
+    <?php
+      $chartStrCnt = array_count_values($dataArr[2]);
+      $data = [[],[]];
+      $data[0] = array_keys($chartStrCnt);
+      foreach($chartStrCnt as $key => $value){
+        array_push($data[1],$value);
+      }
+     ?>
+    <canvas id="myNobChart" height="210" width="565" style="width:565px;height:210px;"></canvas>
+    <script type="text/javascript">
+      var ctx = document.getElementById("myNobChart");
+      var chartStrCnt = <?php echo json_encode($data[1]); ?>;
+      var labels = <?php echo json_encode($data[0]); ?>;
+      var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: labels,
+              datasets: [{
+                  data: chartStrCnt,
+                  backgroundColor: [
+                      'rgba(3, 140, 12, 0.81)'
+                  ],
+                  borderColor: [
+                      'rgba(3, 140, 12,1)'
+                    ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+            responsive: false,
+            legend:{
+              display: false
+            }
+          }
+      });
+    </script>
     <?php
   }
 ?>
--- status/index.php
+++ status/index.php
@@ -3,15 +3,29 @@
   $op = new Operations();
  ?>
 <div class="row">
-  <?php $lguList = $op->selectQuery("SELECT DISTINCT lgu FROM feedbacks", true); ?>
-  <div class="col-md-2">
+  <?php $lguList = $op->selectQuery("SELECT DISTINCT lgu FROM feedbacks", true);
+        $dates[0] = $op->selectQuery("SELECT date FROM feedbacks ORDER BY _id ASC LIMIT 1", true);
+        $dates[1] = $op->selectQuery("SELECT date FROM feedbacks ORDER BY _id DESC LIMIT 1", true);
+
+        $dates[0] = date('Y-m-d', strtotime($dates[0][0]['date']));
+        $dates[1] = date('Y-m-d', strtotime($dates[1][0]['date']));
+  ?>
+  <div class="col-md-2" style="color:white;">
     <label for="">Municipalities</label>
     <br>
-    <select class="lguSelect form-control" name="">
+    <select class="lguSelect form-control" name="" style="height:45px;">
       <?php foreach($lguList as $key => $value): ?>
         <option value="<?=$value['lgu']?>"><?=$value['lgu']?></option>
       <?php endforeach ?>
     </select>
+
+    <label for="">Date From</label>
+    <br>
+    <input type="date" name="" value="<?=$dates[0]?>" class="form-control date-from">
+    <br>
+    <label for="">Date To</label>
+    <br>
+    <input type="date" name="" value="<?=$dates[1]?>" class="form-control date-to">
   </div>
 
     <div class="col-lg-10" style="overflow-y:auto;">
@@ -19,70 +33,77 @@
       <div class="panel panel-default">
         <div class="panel-heading">
           <i class="fa fa-bar-chart-o fa-fw"></i>
-          Area Chart
+          Customer Satisfaction Chart
         </div>
         <div class="panel-body div-chart" style="height:85%;">
 
         </div>
       </div>
-    </div> 
+    </div>
 
      <div class="col-md-6">
       <div class="panel panel-default">
         <div class="panel-heading">
           <i class="fa fa-gears fa-fw"></i> DOST 8 Services
         </div>
-        <div class="panel-body div-services" style="height:55%;">
-          
+        <div class="panel-body div-services" style="height:55%;padding:0px;">
+
         </div>
       </div>
-    </div> 
+    </div>
 
-    <!-- <div class="col-md-5" style="height:90%;">
+    <div class="col-md-6" style="height:90%;margin-top:-85px;">
       <div class="panel panel-default">
         <div class="panel-heading">
           <i class="fa fa-comments"></i>
           Comments
         </div>
-        <div class="panel-body">
-
+        <div class="panel-body div-comments" style="height:320px;padding:0px;">
         </div>
       </div>
-    </div> -->
+    </div>
 
-    <!-- <div class="col-md-5" style="height:40.1%;width:40.9%;position:absolute;margin:24.4% 16.4%;">
+    <div class="col-md-6" style=>
       <div class="panel panel-default">
         <div class="panel-heading">
           <i class="fa fa-briefcase fa-fw"></i>
-          Nature of Business
+          Nature of Business Availed
         </div>
-        <div class="panel-body div-nob" style="height:265px;">
+        <div class="panel-body div-nob" style="height:235px;">
 
         </div>
       </div>
-    </div>-->
-  </div> 
+    </div>
+  </div>
 </div>
 
 <script type="text/javascript">
   $(document).ready(function(){
-    $.ajax({
-      url : 'spec_func.php',
-      data : 'type=view_chart&lgu='+$('.lguSelect option:first').val()
-    }).done(function(r){
-      $('.div-chart').html(r);
-    });
+    $.fn.fetchRecord = function(init = false){
+      var lgu = $('.lguSelect option:first').val();
+      if(!init){
+        lgu = $(this).val();
+      }
 
-    $('.lguSelect').on('change',function(){
       $('.div-chart, .div-services').html('<img src="images/clipart/load.gif" style="width:200px;">');
       $.ajax({
         url : 'spec_func.php',
-        data : 'type=viewChart&lgu='+$(this).val()
+        data : 'type=viewChart&lgu='+lgu+'&fromDate='+$('.date-from').val()+'&toDate'+$('.date-to').val()
       }).done(function(res){
         var res = res.split('<!--BREAK-->');
         $('.div-chart').html(res[0]);
         $('.div-services').html(res[1]);
+        $('.div-comments').html(res[2]);
+        $('.div-nob').html(res[3]);
       });
+    }
+
+    $.fn.fetchRecord(true);
+
+    $('.lguSelect').on('change',function(){
+      $.fn.fetchRecord();
     });
+
+
   });
 </script>

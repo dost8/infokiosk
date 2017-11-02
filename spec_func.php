ac26109a338f5a1ecc9bab3ff485863b9e62b65e
dost8 <dost8.infokiosk@gmail.com>
2017-11-02 14:48:30 +0800

    updates

--- spec_func.php
+++ spec_func.php
@@ -15,9 +15,9 @@ ini_set('max_execution_time',0);
   }
 
   if($_GET['type'] == 'updateKiosk'){
-    #$operation->cloud_backup();
-    #$result = $operation->execBatchFile('downloadUpdates.bat');
-    $operation->textToDirectDB();die;
+    $operation->cloud_backup();
+    $result = $operation->execBatchFile('downloadUpdates.bat');
+    $operation->textToDirectDB();
     if(strpos($result, 'Already up-to-date.'))
       echo '';
   }
--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
 echo on
 git pull origin master
-git add resources/
+git add *, resources/
 git commit -m "updates"
 git push -u origin master
 exit
